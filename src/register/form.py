6b1782252b3ff166291cd8896d0f9497d2024545
Lynnsanity <lynnellesaavedra8@gmail.com>
2024-03-23 18:10:58 -0400

    form validation steps

--- src/register/form.py
+++ src/register/form.py
@@ -1,13 +1,39 @@
 from nicegui import app, ui
 
+from nicegui import ui
+
+class Enable:
+    def __init__(self) -> None:
+        self.inputs = {}
+        self.no_errors = True
+
+    def is_not_empty(self, v):
+        return v and len(v) > 0
+
+    def on_change(self, e):
+        valid = self.is_not_empty(e.value)
+        self.inputs[e.sender.id] = valid
+        self.update()
+
+    def update(self):
+        for i in self.inputs.values():
+            if i is not True:
+                self.no_errors = False
+                break
+        else:
+            self.no_errors = True
+
+async def submit_registration():
+    ui.notify('it gets this far')
 
 def form():
+    enable = Enable()
     with ui.column().classes('flex items-center justify-center text-center \
                              w-full md:w-3/4 lg:w-1/2 xl:w-1/2 2xl:w-1/2 mt-10 font-mono'):
 
         ui.label('Team Information').classes('w-full flex text-start text-lg text-[#ffc82e]')
         # team name
-        team_name = ui.input('Team Name *') \
+        team_name = ui.input('Team Name *', on_change=enable.on_change, validation={"Required field": enable.is_not_empty}) \
             .props('outlined v-model="text" color=amber') \
             .classes('w-full')
 
@@ -136,3 +162,10 @@ def form():
         agree_checkbox = ui.checkbox('By selecting this checkbox, you and your team acknowledge \
         that you all have read and agree to abide by the rules outlined on the Rules page.') \
             .classes('text-bold')
+
+        #register_button = ui.button('Submit Registration', on_click=lambda: submit_registration() if enable.no_errors else None)
+
+        register_button = ui.button('Submit Registration', on_click=submit_registration)
+        register_button.bind_enabled_from(enable, "no_errors")
+
+
