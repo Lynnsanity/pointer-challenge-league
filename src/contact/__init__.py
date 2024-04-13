c1943ab5fe295400fd9c0eea91e0f7cf1b7c582c
Lynnsanity <lynnellesaavedra8@gmail.com>
2024-04-13 17:28:04 -0400

    enhanced contact form validation

--- src/contact/__init__.py
+++ src/contact/__init__.py
@@ -1,6 +1,7 @@
 from nicegui import app, ui
-from contact.logic import Enable
+from components.logic import Enable
 from components.header_footer import header_footer
+from contact.validator import contact_submit_click
 
 @ui.page('/contact')
 async def contact_page():
@@ -16,20 +17,23 @@ async def contact_page():
                 ui.label('Have questions, comments, concerns and/or feedback?').classes('mt-5 font-mono text-xl')
                 ui.label('Feel free to send us a message').classes('font-mono text-xl')
                 first_name = ui.input('First Name', on_change=enable.on_change,
-                                      validation={"Required field":enable.is_not_empty}) \
+                                      validation={"Required field":enable.empty, "Character Limit Exceeded":enable.char_limit}) \
                     .props('outlined v-model="text" color=amber dark') \
                     .classes('w-full mt-5')
                 last_name = ui.input('Last Name', on_change=enable.on_change, \
-                                     validation={"Required field": enable.is_not_empty}) \
+                                     validation={"Required field": enable.empty, "Character Limit Exceeded":enable.char_limit}) \
                     .props('outlined v-model="text" color=amber dark') \
                     .classes('w-full')
                 email_address = ui.input('Email Address', on_change=enable.on_change, \
-                                         validation={"Required field": enable.is_not_empty, "Invalid Email Address": enable.is_valid_email}) \
+                                         validation={"Required field": enable.empty, "Invalid Email Address":enable.is_valid_email,
+                                                     "Character Limit Exceeded":enable.char_limit}) \
                     .props('outlined v-model="text" color=amber dark') \
                     .classes('w-full')
-                contact_message_summary = ui.textarea('Message Summary', on_change=enable.on_change, \
-                                                      validation={"Required field": enable.is_not_empty, "Too many characters, max is 256": enable.not_many_char}) \
+                message_summary = ui.textarea('Message Summary', on_change=enable.on_change, \
+                                                      validation={"Required field": enable.empty, "Too many characters, max is 512": enable.message_char_limit}) \
                     .props('outlined v-model="text" color=amber dark') \
                     .classes('w-full')
-                contact_button = ui.button('Submit').props('color=amber text-color=black').classes('text-lg')
+                #contact_button = ui.button('Submit').props('color=amber text-color=black').classes('text-lg')
+                #contact_button.bind_enabled_from(enable, "no_errors")
+                contact_button = ui.button('Submit', on_click=lambda: contact_submit_click(first_name.value.strip(), last_name.value.strip(), email_address.value.strip(), message_summary.value.strip())).props('color=amber text-color=black').classes('text-lg')
                 contact_button.bind_enabled_from(enable, "no_errors")
