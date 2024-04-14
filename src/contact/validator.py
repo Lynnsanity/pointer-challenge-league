bacc8bfdcc92cd7d07e0ef16ea7d3e9555c8e17a
Lynnsanity <lynnellesaavedra8@gmail.com>
2024-04-13 22:02:46 -0400

    contact notes and send ticket receipt to user too

--- src/contact/validator.py
+++ src/contact/validator.py
@@ -14,42 +14,108 @@ async def contact_submit_click(first_name, last_name, email_address, message_sum
     }
     errors = []
 
-    # Validate each input individually
+    # this for loop takes each field (input) and validates each individually
+    # this is so that we can make sure even after they hit submit that it
+    # was filled out properly.
     for field, (label, value) in fields.items():
         if not enable.empty(value):
             errors.append(f"Please enter your {label}.")
-        elif not enable.char_limit(value):
-            if field == "message_summary":
-                if not enable.message_char_limit(value):
-                    errors.append("There are too many characters in {label}. Max is 512.")
-                else:
-                    pass
-            else:
+        elif not field == "message_summary":
+            if not enable.char_limit(value):
                 errors.append(f"There are too many characters in {label}.")
+        elif field == "message_summary":
+            if not enable.message_char_limit(value):
+                errors.append("There are too many characters in {label}. Max is 512.")
         elif field == "email_address" and not enable.is_valid_email(value):
             errors.append("Please enter a valid email address.")
+        else:
+            pass
 
+    # if any things went bad in that for loop, we'll notify the user and do nothing else
     if errors:
         for error in errors:
             ui.notify(error, type="error")
+
+    # if there's no errors, send the contact form in an email to uwsp esports
     else:
-        email_sent = await contact_message_received(
+        contact_email_sent = await contact_message_received(
             first_name=first_name,
             last_name=last_name,
             email_address=email_address,
             message_summary=message_summary)
-
-        if email_sent:
-            ui.notify(f"Successfully sent ticket. We will get back to you at {email_address} as soon as possible",
-                      type='positive',
-                      color='purple-9')
+        # if that email sent then let the user know, if it didn't send then we got a prrrroblem
+        if contact_email_sent:
+            confirmation_sent = await contact_receipt(
+                first_name=first_name,
+                last_name=last_name,
+                email_address=email_address,
+                message_summary=message_summary
+            )
+            if confirmation_sent:
+                ui.notify(f"Successfully sent support ticket. We will get back to you at {email_address} as soon as possible.",
+                          type='positive',
+                          color='purple-9')
+            else:
+                ui.notify(f"Successfully sent support ticket. Failed to send receipt.",
+                          f"We will get back to you at {email_address} as soon as possible.",
+                          type='positive',
+                          color='purple-9')
         else:
-            ui.notify("We could not receive your message. Please try again later.", type="negative")
+            ui.notify("Failed to send support ticket."
+                      "We are experiencing technical difficulties, please try again later.",
+                      type="negative")
 
-async def contact_message_received(first_name, last_name, email_address, message_summary):
-    email_sender = 'uwspsupportemailhere'
+# the format of the email to support and some env variables needed to then use sender to send email
+async def contact_receipt(first_name, last_name, email_address, message_summary):
+    email_sender = 'someuwspemail@info.org'
     email_password = os.environ.get('EMAIL_PASS')
-    email_receiver = 'supportemailhere'
+    email_receiver = email_address
+    subject = "Contact Support Receipt"
+    body = f'''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <link rel="stylesheet" type="text/css" hs-webfonts="true" href="https://fonts.googleapis.com/css?family=Lato|Lato:i,b,bi">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body bgcolor="#F5F8FA" style="width: 100%; font-family:Lato, sans-serif; font-size:18px;">
+    <div id="email">
+        <table role="presentation" border="0" cellpadding="0" cellspacing="10px" style="padding: 30px 30px 30px 60px;">
+            <tr>
+                <td>
+                    <h2>PCL OW 2 Support Contact Receipt</h2>
+                    <p>
+                        Here is your copy of the support ticket submitted.<br>
+                        We will get back to you as soon as we can.<br>
+                    </p>
+                    <p>
+                        From: {first_name} {last_name}<br>
+                        Email: {email_address}<br>
+                        Message: {message_summary}<br>
+                    </p>
+                    <p>
+                    Thank you for your patience,<br>
+                    The PCL Team
+                </td>
+            </tr>
+        </table>
+    </div>
+    </body>
+    </html>
+    '''
+    return send_email(
+    email_sender=email_sender,
+    email_password=email_password,
+    email_receiver=email_receiver,
+    subject=subject,
+    body=body)
+
+# the format of the email to support and some env variables needed to then use sender to send email
+async def contact_message_received(first_name, last_name, email_address, message_summary):
+    email_sender = 'senderdummyemail'
+    email_password = os.environ.get('EMAIL_PASS')
+    email_receiver = 'uwspsupport'
     subject = "Contact Support Request"
     body = f'''
     <!DOCTYPE html>
@@ -66,13 +132,9 @@ async def contact_message_received(first_name, last_name, email_address, message
                 <td>
                     <h2>PCL Contact Support Message</h2>
                     <p>
-                        From: {first_name} {last_name}
-                    </p>
-                    <p>
-                        Email: {email_address}
-                    </p>
-                    <p>
-                        Message: {message_summary}
+                        From: {first_name} {last_name}<br>
+                        Email: {email_address}<br>
+                        Message: {message_summary}<br>
                     </p>
                 </td>
             </tr>
