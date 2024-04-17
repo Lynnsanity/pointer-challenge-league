425dcfa3287d5cd3857a042484c32d73cf6ec3bc
Lynnsanity <lynnellesaavedra8@gmail.com>
2024-04-16 20:14:41 -0400

    home page lookin guuud

--- src/home/__init__.py
+++ src/home/__init__.py
@@ -15,54 +15,72 @@ async def home_page():
     carousel()
 
     with ui.row().classes('w-full bg-gradient-to-r from-yellow-500 to-purple-500 p-1'):
-        ui.image('img/ow2/ramm.webp')
+        ui.image('img/ow2/ram2.jpg')
     #with ui.row().classes('grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mx-auto w-full h-2/3 flex items-center justify-center my-5'):
     #    ui.label('Some text here')
     #    ui.image('img/ow2/home/winston.png').classes('w-auto')
 
-    with ui.row().classes('mx-auto my-5 text-center'):
-        ui.label('GETTING STARTED').classes('text-5xl min-[320px]:text-4xl font-extrabold text-[#FFC82e] font-mono')
+    with ui.row().classes('w-full'):
+        ui.image('img/pcl-logos/getting-started.png').classes('mx-auto w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/3 my-5 flex justify-center')
 
     with ui.row().classes('grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 my-5 w-full'):
-        with ui.card().tight().classes('no-shadow aspect-square bg-transparent'):
-            ui.button(icon='gavel').props('flat color=white').classes('mx-auto w-full text-6xl text-center')
-            #ui.image('img/games/league.png').classes('object-cover')
-        with ui.card().tight().classes('no-shadow aspect-square bg-transparent'):
-            ui.button(icon='sports_esports').props('flat color=white').classes('mx-auto w-full text-6xl text-center')
-        with ui.card().tight().classes('no-shadow aspect-square bg-transparent'):
-            ui.button(icon='feed').props('flat color=white').classes('mx-auto w-full text-6xl text-center')
-            ui.label('Example').classes('w-full text-center')
+        with ui.link(target='/rules').classes('no-underline'):
+            with ui.card().tight().classes('no-shadow aspect-square bg-[#512698]'):
+                ui.icon('gavel').props('flat color=white').classes('mx-auto w-full mt-10 text-9xl text-center')
+                ui.image('img/pcl-logos/rules.png').classes('mx-auto my-5 w-1/3 flex justify-center')
+                with ui.column().classes('w-full p-5'):
+                    ui.label('Fair play is at the core of our community. '
+                             'Before you dive into the action, take a moment to familiarize yourself '
+                             'with our rules. Click here to explore further details. ') \
+                        .classes('text-lg text-[#ffc82e] px-5 font-mono font-bold')
+        with ui.link(target='/register').classes('no-underline'):
+            with ui.card().tight().classes('no-shadow aspect-square bg-[#FFC82E]'):
+                ui.icon('feed').props('flat color=white').classes('mx-auto w-full mt-10 text-9xl text-center')
+                ui.image('img/pcl-logos/register-purple.png').classes('mx-auto my-5 w-1/2 flex justify-center')
+                with ui.column().classes('w-full p-5'):
+                    ui.label("Already understand the rules? Great! "
+                             "Take the next step by clicking here to fill "
+                             "out the application for your team's gaming sessions. "
+                             "Let's get you in the game – register now! ") \
+                        .classes('text-lg text-[#512698] px-5 font-mono font-bold')
+        with ui.link(target='/standings').classes('no-underline'):
+            with ui.card().tight().classes('no-shadow aspect-square bg-[#512698]'):
+                ui.icon('emoji_events').props('flat color=white').classes('mx-auto w-full mt-10 text-9xl text-center')
+                ui.image('img/pcl-logos/standings.png').classes('mx-auto my-5 w-3/5 flex justify-center')
+                with ui.column().classes('w-full p-5'):
+                    ui.label("Curious about where your team stands? Click here to view "
+                             "the latest standings and see how you stack up against the "
+                             "competition. Keep track of your progress and aim for the top spot!") \
+                        .classes('text-lg text-[#ffc82e] px-5 font-mono font-bold')
 
-    with ui.row().classes('mx-auto my-5 text-center'):
-        ui.label('GAMES WE PLAY').classes('text-5xl min-[320px]:text-4xl font-extrabold text-[#FFC82e] font-mono')
+    with ui.row().classes('w-full'):
+        ui.image('img/pcl-logos/games-we-play.png').classes('mx-auto w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/3 my-5 flex justify-center')
 
 
     with ui.row().classes('grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 w-full justify-center items-center text-center'):
-        
             with ui.image('img/games/overwatch.jpg') \
-                    .classes('mx-auto transition-all duration-500 ease-in-out hover:scale-105 rounded-lg \
-                             aspect-square w-4/5 no-shadow bg-transparent border-[#512698] border-2 mt-5'):
-                with ui.card() \
-                        .props('flat unelevated') \
-                        .classes('w-full h-full opacity-0 hover:opacity-100 flex justify-center items-center text-center'):
-                    ui.label('Overwatch 2') \
-                        .classes('w-full text-3xl font-mono font-bold text-black')
-
-            with ui.image('img/games/fortnite.jpeg') \
                     .classes('mx-auto transition-all duration-500 ease-in-out hover:scale-105 rounded-lg \
                              aspect-square w-4/5 no-shadow bg-transparent border-[#ffc82e] border-2 mt-5'):
                 with ui.card() \
                         .props('flat unelevated') \
                         .classes('w-full h-full opacity-0 hover:opacity-100 flex justify-center items-center text-center'):
-                    ui.label('Fortnite').classes('w-full text-3xl font-mono font-bold text-black')
+                    ui.image('img/ow2/ow2logo2.png').classes('w-full')
 
-            with ui.image('img/games/league.png') \
+            with ui.image('img/games/fortnite.jpeg') \
                     .classes('mx-auto transition-all duration-500 ease-in-out hover:scale-105 rounded-lg \
                              aspect-square w-4/5 no-shadow bg-transparent border-[#512698] border-2 mt-5'):
                 with ui.card() \
                         .props('flat unelevated') \
                         .classes('w-full h-full opacity-0 hover:opacity-100 flex justify-center items-center text-center'):
-                    ui.label('League of Legends').classes('w-full text-3xl font-mono font-bold text-black')
+                    ui.image('img/games/fortnitelogo.png').classes('w-full')
+
+            with ui.image('img/games/league.png') \
+                    .classes('mx-auto transition-all duration-500 ease-in-out hover:scale-105 rounded-lg \
+                             aspect-square w-4/5 no-shadow bg-transparent border-[#ffc82e] border-2 mt-5'):
+                with ui.card() \
+                        .props('flat unelevated') \
+                        .classes('w-full h-full opacity-0 hover:opacity-100 flex justify-center items-center text-center'):
+                    ui.image('img/games/lollogo2.png').classes('w-full')
 
             with ui.image('img/games/smashbros.jpeg') \
                     .classes('mx-auto transition-all duration-500 ease-in-out hover:scale-105 rounded-lg \
@@ -70,7 +88,7 @@ async def home_page():
                 with ui.card() \
                         .props('flat unelevated') \
                         .classes('w-full h-full opacity-0 hover:opacity-100 flex justify-center items-center text-center'):
-                    ui.label('Super Smash Bros.').classes('w-full text-3xl font-mono font-bold text-black')
+                    ui.image('img/games/ssbu-logo.png').classes('w-full')
 
             with ui.image('img/games/apex.jpeg') \
                     .classes('mx-auto transition-all duration-500 ease-in-out hover:scale-105 rounded-lg \
@@ -78,7 +96,7 @@ async def home_page():
                 with ui.card() \
                         .props('flat unelevated') \
                         .classes('w-full h-full opacity-0 hover:opacity-100 flex justify-center items-center text-center'):
-                    ui.label('Apex Legends').classes('w-full text-3xl font-mono font-bold text-black')
+                    ui.image('img/games/apexlegendslogo2.png').classes('w-full')
 
             with ui.image('img/games/rocketleague.jpeg') \
                     .classes('mx-auto transition-all duration-500 ease-in-out hover:scale-105 rounded-lg \
@@ -88,8 +106,8 @@ async def home_page():
                         .classes('w-full h-full opacity-0 hover:opacity-100 flex justify-center items-center text-center'):
                     ui.image('img/games/rocketleaguelogo.webp').classes('w-full')
 
-    with ui.row().classes('mx-auto my-5 text-center'):
-        ui.label('CONTACT US').classes('text-5xl min-[320px]:text-4xl font-extrabold text-[#FFC82e] font-mono')
+    with ui.row().classes('w-full'):
+        ui.image('img/pcl-logos/contact-us.png').classes('mx-auto w-3/4 md:w-1/2 lg:w-1/4 xl:w-1/4 my-5 flex justify-center')
 
 
 
