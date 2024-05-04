f2a0300ce02c15bc815537034749087c01062c54
Lynnsanity <lynnellesaavedra8@gmail.com>
2024-05-04 01:26:38 -0400

    supporting team logo in email

--- src/components/sender.py
+++ src/components/sender.py
@@ -3,8 +3,9 @@ from email.message import EmailMessage
 import os
 import ssl
 import smtplib
+import base64
 
-def send_email(email_sender, email_password, email_receiver, subject, body):
+def send_email(email_sender, email_password, email_receiver, subject, body, image_data=None):
     msg = EmailMessage()
     msg['From'] = email_sender
     msg['To'] = email_receiver
@@ -13,6 +14,9 @@ def send_email(email_sender, email_password, email_receiver, subject, body):
 
     context = ssl.create_default_context()
 
+    if image_data:
+            image_attachment = base64.b64decode(image_data)
+            msg.add_attachment(image_attachment, maintype='image', subtype='png', filename='team_logo.png')
     try:
         with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
             smtp.login(email_sender, email_password)
