fd9589428916d85cd53753a257a1d84870e9edfb
Lynnsanity <lynnellesaavedra8@gmail.com>
2024-05-03 23:57:27 -0400

    header looks a little better like this on md devices

--- src/components/header_footer.py
+++ src/components/header_footer.py
@@ -36,20 +36,21 @@ discord_logo = '''
 
 def header_footer():
     with ui.header(elevated=True).style('background-color: #181818').classes('w-full items-center justify-between fixed'):
-        with ui.link('', '/home'):
-            ui.image('./img/pcl-logos/glow-pcl.png').classes('w-[270px]')
-        with ui.row().classes('max-sm:hidden font-mono'):
-            ui.button('Rules', icon='gavel', on_click=lambda: ui.open('/rules')).props('flat color=white')
-            ui.button('Register', icon='feed', on_click=lambda: ui.open('/register')).props('flat color=white')
-            ui.button('Standings', icon='emoji_events', on_click=lambda: ui.open('/standings')).props('flat color=white')
-            ui.button('Contact', icon='contact_support', on_click=lambda: ui.open('/contact')).props('flat color=white')
-        with ui.row().classes('sm:hidden'):
-            with ui.button(icon='menu').props('flat color=white'):
-                with ui.menu().style('background-color: #181818').classes('text-white border-2 border-purple-500 border-opacity-50 font-mono') as menu:
-                    ui.menu_item('Rules', on_click=lambda: ui.open('/rules')).classes('px-5 py-3 flex justify-center text-center')
-                    ui.menu_item('Register', on_click=lambda: ui.open('/register')).classes('px-5 py-3flex justify-center text-center')
-                    ui.menu_item('Standings', on_click=lambda: ui.open('/standings')).classes('px-5 py-3 flex justify-center text-center')
-                    ui.menu_item('Contact', on_click=lambda: ui.open('/contact')).classes('px-5 py-3 flex justify-center text-center')
+        with ui.row().classes('w-full grid grid-cols-2'):
+            with ui.link('', '/home'):
+                ui.image('./img/pcl-logos/glow-pcl.png').classes('w-[270px]')
+            with ui.row().classes('w-full max-sm:hidden font-mono justify-end items-end'):
+                ui.button('Rules', icon='gavel', on_click=lambda: ui.open('/rules')).props('flat color=white')
+                ui.button('Register', icon='feed', on_click=lambda: ui.open('/register')).props('flat color=white')
+                ui.button('Standings', icon='emoji_events', on_click=lambda: ui.open('/standings')).props('flat color=white')
+                ui.button('Contact', icon='contact_support', on_click=lambda: ui.open('/contact')).props('flat color=white')
+            with ui.row().classes('w-full sm:hidden justify-end items-end'):
+                with ui.button(icon='menu').props('flat color=white'):
+                    with ui.menu().style('background-color: #181818').classes('text-white border-2 border-purple-500 border-opacity-50 font-mono') as menu:
+                        ui.menu_item('Rules', on_click=lambda: ui.open('/rules')).classes('px-5 py-3 flex justify-center text-center')
+                        ui.menu_item('Register', on_click=lambda: ui.open('/register')).classes('px-5 py-3 flex justify-center text-center')
+                        ui.menu_item('Standings', on_click=lambda: ui.open('/standings')).classes('px-5 py-3 flex justify-center text-center')
+                        ui.menu_item('Contact', on_click=lambda: ui.open('/contact')).classes('px-5 py-3 flex justify-center text-center')
 
     with ui.footer(fixed=False).style('background-color: #181818').classes('w-full border-t-2 border-[#512698]'):
         with ui.row().classes('mx-auto w-full'):
